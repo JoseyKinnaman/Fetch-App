cf47b193981917dd1abea370b90c6bbc538c5ab7
Jozy Kinnaman <jozypants@gmail.com>
2020-07-28 11:28:12 -0700

    set up api call for retreiving a random name.

--- src/components/App.js
+++ src/components/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="ui container" style={{marginTop: '10px'}}>
       <div id="header-panel" className="ui segment">
-        <img src={logo}/>
+        <img src={logo} alt="Fetch logo with dog and human"/>
       
       <h2>A fun and hip companion animal naming  tool for shelters and humans.</h2>
       </div>
--- src/components/RadomButton.js
+++ src/components/RadomButton.js
@@ -1,42 +1,34 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './RandomButton.css'
 
-
+const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 function RandomButton() {
-  const [loading, setLoading] = React.useState(true);
-  const [name, setName] = React.useState("Loading...");
-  const [value, setValue] = React.useState();
+  const [loading, setLoading] = useState(true);
+  const [randomName, setRandomName] = useState([]);
+  // const [currentRadomName, setCurrentRandomName] = useState();
 
-  React.useEffect(() => {
-    let unmounted = false;
+  useEffect(() => {
+    setLoading(true)
     async function getRandom() {
-      const proxyurl = "https://cors-anywhere.herokuapp.com/";
-      const url = "https://pet-name-api.herokuapp.com/names/random"
-      const response = await fetch(proxyurl + url)
+      const response = await fetch(`${proxyurl}https://pet-name-api.herokuapp.com/names/random`)
       const body = await response.json();
       console.log(body)
-      if (!unmounted) {
-        setName(
-          body.name, body.comment
-        );
-        setLoading(false);
-      }
+      setRandomName(body);
+      setLoading(false);
     }
     getRandom();
-    return () => {
-      unmounted = true
-    };
   }, []);
   return (
     <div id="panel" className="ui segment">
       <h3>Get a random pet name suggestion:</h3>
       <button>Random</button>
-      <h5
-        disabled={loading}
-        value={value}>
-          {value}</h5>
-          
+      {/* {randomName.map(name => <div key={name.id}> */}
+        <ul>
+          <li><h5>{randomName.name}</h5>
+          <p>{randomName.comment}</p></li>
+        </ul>
+      {/* </div>)} */}
     </div>
 
   );
