a2e122e9513737aa14010ef8af55bfea63058848
Jozy Kinnaman <jozypants@gmail.com>
2020-07-28 14:40:12 -0700

    update API call and remove hooks to work with button click.

--- src/components/RadomButton.js
+++ src/components/RadomButton.js
@@ -1,27 +1,22 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState } from 'react';
 import './RandomButton.css'
 
 const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
 function RandomButton() {
-  
   const [randomName, setRandomName] = useState([]);
-
-  useEffect(() => {
-    async function getRandom() {
-      const response = await fetch(`${proxyurl}https://pet-name-api.herokuapp.com/names/random`)
-      const body = await response.json();
-      setRandomName(body);
-    }
-    getRandom();
-  }, []);
+  const getRandom = async () => {
+    const response = await fetch(`${proxyurl}https://pet-name-api.herokuapp.com/names/random`)
+    const body = await response.json();
+    setRandomName(body);
+  }
   return (
     <div id="panel" className="ui segment">
       <h3>Get a random pet name suggestion:</h3>
-      <button OnClick={useEffect}>Random </button>
-        <hr/>
-        <h5>{randomName.name}</h5>
-        <p>{randomName.comment}</p>
+      <button onClick={getRandom}  >Random </button>
+      <hr />
+      <h5>{randomName.name}</h5>
+      <p>{randomName.comment}</p>
     </div>
   );
 }
