9217d47a64e9b0af76d0a5c73516647b66aba77d
Jozy Kinnaman <jozypants@gmail.com>
2020-07-29 13:45:10 -0700

    update popout text. Experiment with columns.

--- src/components/App.js
+++ src/components/App.js
@@ -12,14 +12,14 @@ function App() {
       <div id="header-panel" className="ui segment">
         <img src={logo} alt="Fetch logo with dog and human"/>
       <h3>An companion animal naming tool for shelters and humans that is... well, a little outside of the box.</h3>
-        <Popup trigger={<button class="ui button"> Bark!</button>} position="right center">
-          <div><p>Fetch was started with the idea that all companion animals are special and deserve names that fit their unique personalities! Originally intented as a book, Jozy Kinnaman began collecting pet names over 10 years ago after working in various animal shelters and hospitals. This app is meant to inspire creativity and assist in the companion animal naming process. We have omitted gender but some names may be more fitting than others. We also urge you to consider cultural context with the names and choose appropriately. Best of luck!</p></div>
+        <Popup trigger={<button class="ui button"> Bark!</button>} position="center">
+          <div><h5>Fetch was started with the idea that all companion animals are special and deserve names that fit their unique personalities! Originally intented as a book, Jozy Kinnaman began collecting pet names over 10 years ago after working in various animal shelters and hospitals. This app is meant to inspire creativity and assist in the companion animal naming process. We have omitted gender but some names may be more fitting than others. We also urge you to consider cultural context with the names and choose appropriately. Best of luck!</h5></div>
         </Popup>
       </div>
         <RandomButton />
         <DropDown/>
-      <Popup trigger={<button class="ui button"> Meow!</button>} position="right center">
-        <div>Fetcher is an open source API! If you would like to contribute, shoot us an email or visit our github! !!</div>
+      <Popup trigger={<button class="ui button"> Meow!</button>} position="center">
+        <h5>Fetcher is an open source API! If you would like to contribute, shoot us an email or visit our github!!!</h5>
       </Popup>
     </div>
   );
